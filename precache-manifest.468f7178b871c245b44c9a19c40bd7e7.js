b3a4a574aea8e8456ea22d2b627d3fb44392b533
s19007 <s19007@std.it-college.ac.jp>
2020-02-25 18:37:56 +0000

    Updates

---
+++ precache-manifest.468f7178b871c245b44c9a19c40bd7e7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "32e82bd4d6b86ffddd8dab4075449b08",
+    "url": "/kadai2/index.html"
+  },
+  {
+    "revision": "391b8f6947f49c9f1243",
+    "url": "/kadai2/static/css/main.e012f8b4.chunk.css"
+  },
+  {
+    "revision": "4c35d34f17d4aec7a039",
+    "url": "/kadai2/static/js/2.b25a4acb.chunk.js"
+  },
+  {
+    "revision": "3adc01bea76e7956dc3633ee898f6936",
+    "url": "/kadai2/static/js/2.b25a4acb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "391b8f6947f49c9f1243",
+    "url": "/kadai2/static/js/main.cb297236.chunk.js"
+  },
+  {
+    "revision": "42cd405585bf1af6987c",
+    "url": "/kadai2/static/js/runtime-main.49f0e3dc.js"
+  }
+]);
